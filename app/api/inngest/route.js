25a8bff0368a84c35bf10684c6af24098400ea26
Abhishek Nigam <abhisheknigam755@gmail.com>
2025-02-05 10:08:31 +0530

    preparing for deployment

---
+++ app/api/inngest/route.js
@@ -0,0 +1,19 @@
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/functions";
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
+    checkBudgetAlerts,
+  ],
+});
